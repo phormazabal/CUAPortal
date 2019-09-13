e1f388d5d64567e41a0340dd5f7e5a7b814d47f8
pablohormazabal <phormazabal@clinicauandes.cl>
2019-09-13 12:59:08 -0300

    11-09-2019 V2

--- PortalPrivado.BO/Paciente.cs
+++ PortalPrivado.BO/Paciente.cs
@@ -14,6 +14,8 @@ namespace PortalPrivado.BO
         String _Telefono1;
         String _Telefono2;
         String _Password;
+        DateTime _FechaNacimiento;
+        String _Rut;
 
         public string Nombre { get => _Nombre; set => _Nombre = value; }
         public string Apellidos { get => _Apellidos; set => _Apellidos = value; }
@@ -21,5 +23,7 @@ namespace PortalPrivado.BO
         public string Telefono1 { get => _Telefono1; set => _Telefono1 = value; }
         public string Telefono2 { get => _Telefono2; set => _Telefono2 = value; }
         public string Password { get => _Password; set => _Password = value; }
+        public DateTime FechaNacimiento { get => _FechaNacimiento; set => _FechaNacimiento = value; }
+        public string Rut { get => _Rut; set => _Rut = value; }
     }
 }
--- PortalPrivado.DAO/PacienteDao.cs
+++ PortalPrivado.DAO/PacienteDao.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PortalPrivado.BO;
 
 namespace PortalPrivado.DAO
 {
-    class PacienteDao
+    public class PacienteDao
     {
+        /// <summary>
+        /// Retorna la información relacionada a un paciente, tiene como
+        /// parametro de entrada el documento de indentificación(Rut Passport)
+        /// </summary>
+        /// <returns>Objeto con la información de un paciente</returns>
+        public Paciente GetPaciente(String DocId)
+        {
+            Paciente objPaciente = new Paciente();          
+            
+            return objPaciente;
+
+        }
+        
     }
 }
