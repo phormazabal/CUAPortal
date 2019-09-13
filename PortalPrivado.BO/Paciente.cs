e23194e3e556493ea7f91dc1fa2b90272aba50e8
PABLO ANDRES HORMAZABAL <phormazabal@clinicauandes.cl>
2019-09-13 12:45:20 -0300

    13-09-2019

--- PortalPrivado.BO/Paciente.cs
+++ PortalPrivado.BO/Paciente.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace PortalPrivado.BO
 {
-    class Paciente
+    public class Paciente
     {
+        String _Nombre;
+        String _Apellidos;
+        String _Email;
+        String _Telefono1;
+        String _Telefono2;
+        String _Password;
+
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Apellidos { get => _Apellidos; set => _Apellidos = value; }
+        public string Email { get => _Email; set => _Email = value; }
+        public string Telefono1 { get => _Telefono1; set => _Telefono1 = value; }
+        public string Telefono2 { get => _Telefono2; set => _Telefono2 = value; }
+        public string Password { get => _Password; set => _Password = value; }
     }
 }
