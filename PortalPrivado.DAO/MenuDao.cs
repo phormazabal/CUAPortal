706e9a72d3de5b3d2c02248d78293f11e6a5e15f
PABLO ANDRES HORMAZABAL <phormazabal@clinicauandes.cl>
2019-09-23 14:17:39 -0300

    23092019v5

--- PortalPrivado.DAO/MenuDao.cs
+++ PortalPrivado.DAO/MenuDao.cs
@@ -12,12 +12,13 @@ namespace PortalPrivado.DAO
         {
             try
             {
+                Config oConfig = new Config();
                 List<Menu> lstMenu = new List<Menu>();
                 WsMenu.SI_os_GetMenuService serv = new WsMenu.SI_os_GetMenuService();
                 WsMenu.DT_r_GetMenuMenu[] GetMenu;
                 WsMenu.DT_GetMenu dT_GetMenu = new WsMenu.DT_GetMenu();
                 dT_GetMenu.IdMenu = "";
-                serv.Credentials = new NetworkCredential("phormazabal", "Clinica2019");
+                serv.Credentials = new NetworkCredential(oConfig.User, oConfig.Pass);
                 GetMenu = serv.SI_os_GetMenu(dT_GetMenu);
                 serv.Dispose();
 				for (int i = 0; i < GetMenu.Length; i++)
@@ -25,6 +26,8 @@ namespace PortalPrivado.DAO
 					Menu objMenu = new Menu();
 					objMenu.IdMenu = GetMenu[i].IdMenu;
 					objMenu.GlosaMenu = GetMenu[i].Glosa;
+                    objMenu.Url = GetMenu[i].URL;
+                    lstMenu.Add(objMenu);
 				}
                 return lstMenu;
             }
---
+++ PortalPrivado.Web/PortalM.Master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PortalPrivado.BO;
+using PortalPrivado.DAO;
+namespace PortalPrivado.Web
+{
+    public partial class PortalM : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                MenuDao oMenudao = new MenuDao();
+                
+                RpMenu.DataSource = oMenudao.GetMenu();
+                RpMenu.DataBind();
+            }
+        }
+    }
+}
